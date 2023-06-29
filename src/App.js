44bfdf7fd6f58386c6d3b6c00fb395b4542ab9ff
ProjectTracker <132274425+DebuCode@users.noreply.github.com>
2023-06-29 18:09:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import News from './News';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+export default class App extends Component {
+  pageSize = 6;
+  render() {
+    return (   
+      
+          <Router>  
+          <Navbar/>
+          <div>          
+          <Routes>          
+       <Route  path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
+       <Route  path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business"/>}/>
+       <Route  path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>}/>
+       <Route  path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health"/>}/>
+       <Route  path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science"/>}/>
+       <Route  path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="technology"/>}/>
+       <Route  path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="sports"/>}/>
+       <Route  path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general"/>}/>
+         </Routes>
+         </div>
+         </Router>
+        
+      
+    )
+  }
+}
+
